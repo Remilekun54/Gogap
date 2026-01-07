8bfd2f1db341235e90aa80fb37c9946f75778a2b
Remilekun54 <olaniyanremilekundesmond@gmail.com>
2026-01-07 18:56:22 +0100

    last commit

--- website/admin.py
+++
@@ -1,9 +0,0 @@
-
-# Register your models here.
-from django.contrib import admin
-from .models import HeroSlide
-
-@admin.register(HeroSlide)
-class HeroSlideAdmin(admin.ModelAdmin):
-    list_display = ('title', 'order', 'is_active')
-    list_editable = ('order', 'is_active')
